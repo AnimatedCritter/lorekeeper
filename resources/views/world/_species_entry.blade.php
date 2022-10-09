5bf5b72fab99c805e76d4d442070966ac551195d
DESKTOP-01VSOB5\Safira Island <animatedcritter@gmail.com>
2022-10-08 23:52:46 -0500

    Feat: dispaly if species/subtype is_free_myo_usable on World Page

--- resources/views/world/_species_entry.blade.php
+++ resources/views/world/_species_entry.blade.php
@@ -3,6 +3,14 @@
         <div class="col-md-3 world-entry-image"><a href="{{ $species->speciesImageUrl }}" data-lightbox="entry" data-title="{{ $species->name }}"><img src="{{ $species->speciesImageUrl }}" class="world-entry-image" alt="{{ $species->name }}" /></a></div>
     @endif
     <div class="{{ $species->speciesImageUrl ? 'col-md-9' : 'col-12' }}">
+        @if($species->is_free_myo_usable)
+            <div class="float-right m-2">
+                    <span class="fa-stack fa-1x" data-toggle="tooltip" data-placement="top" title="This species can be selected for Free MYO slots!">
+                        <i class="fas fa-ticket-alt fa-stack-2x"></i>
+                        <i class="fas fa-gift fa-xs fa-stack-1x fa-inverse"></i>
+                    </span>
+            </div>
+        @endif
         <h3>{!! $species->displayName !!} <a href="{{ $species->searchUrl }}" class="world-entry-search text-muted"><i class="fas fa-search"></i></a></h3>
         @if(count($species->features) && Config::get('lorekeeper.extensions.species_trait_index'))
             <a href="{{ $species->visualTraitsUrl }}"><strong>Visual Trait Index</strong></a>
--- resources/views/world/_subtype_entry.blade.php
+++ resources/views/world/_subtype_entry.blade.php
@@ -3,6 +3,14 @@
         <div class="col-md-3 world-entry-image"><a href="{{ $subtype->subtypeImageUrl }}" data-lightbox="entry" data-title="{{ $subtype->name }}"><img src="{{ $subtype->subtypeImageUrl }}" class="world-entry-image" alt="{{ $subtype->name }}" /></a></div>
     @endif
     <div class="{{ $subtype->subtypeImageUrl ? 'col-md-9' : 'col-12' }}">
+        @if($subtype->is_free_myo_usable)
+            <div class="float-right m-2">
+                    <span class="fa-stack fa-1x" data-toggle="tooltip" data-placement="top" title="This species can be selected for Free MYO slots!">
+                        <i class="fas fa-ticket-alt fa-stack-2x"></i>
+                        <i class="fas fa-gift fa-xs fa-stack-1x fa-inverse"></i>
+                    </span>
+            </div>
+        @endif
         <h3>{!! $subtype->displayName !!} ({!! $subtype->species->displayName !!} Subtype) <a href="{{ $subtype->searchUrl }}" class="world-entry-search text-muted"><i class="fas fa-search"></i></a></h3>
         <div class="world-entry-text">
             {!! $subtype->parsed_description !!}
