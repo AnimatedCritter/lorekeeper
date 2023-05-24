80f3eee1c38ca06aafb1c309ef0a57a0a9a50f6b
DESKTOP-01VSOB5\Safira Island <animatedcritter@gmail.com>
2023-05-24 17:26:05 -0500

    Fix: attempt to read "updated_at" on null when updating log-less users

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -125,8 +125,10 @@ class UserService extends Service
             if(!Hash::check($data['password'], $user->password)) throw new \Exception("Please make sure you've entered your password correctly.");
 
             $lastUsernameChange = UsernameLog::where('user_id', $user->id)->where('is_staff_edit', 0)->orderBy('updated_at', 'DESC')->first();
-            $daysSinceNameChange = Carbon::now()->diffInDays($lastUsernameChange->updated_at);
-            if($daysSinceNameChange < Settings::get('username_change_cooldown')) throw new \Exception("You must wait for your cooldown to end before changing your username again.");
+            if($lastUsernameChange) {
+                $daysSinceNameChange = Carbon::now()->diffInDays($lastUsernameChange->updated_at);
+                if($daysSinceNameChange < Settings::get('username_change_cooldown')) throw new \Exception("You must wait for your cooldown to end before changing your username again.");
+            }
 
             if(!$this->createUsernameLog($user->id, $user->name, $data['username'], 0)) throw new \Exception("Failed to create log.");
 
